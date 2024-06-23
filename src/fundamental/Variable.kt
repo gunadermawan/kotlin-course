130b2b58618a92b4ce6733c5a35823c4c71af361
gunadermawan <gunadermawan.official@gmail.com>
2024-06-24 00:07:37 +0700

    course: add variable

---
+++ src/fundamental/Variable.kt
@@ -0,0 +1,20 @@
+package fundamental
+
+fun main() {
+
+//    mutable variable
+    var name = "enigma"
+    name = "superhero"
+    println(name)
+
+//    immutable variable
+    val address = "Jakarta Barat"
+    println(address)
+
+//    late init variable
+    val number: Int
+//    get data from BE
+    number = 0
+
+    println(number)
+}
