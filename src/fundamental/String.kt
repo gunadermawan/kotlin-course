80eefb06751aa7fd5f4ae8c517f3f3123e6e5e83
gunadermawan <gunadermawan.official@gmail.com>
2024-06-29 17:06:41 +0700

    course: String

--- src/fundamental/String.kt
+++ src/fundamental/String.kt
@@ -1,30 +1,30 @@
 package fundamental
 
 fun main() {
-//    declare String
-    val name = "Kotlin"
-    val greetings = "Hello, $name!"
+    // declare String
+    val name: String = "Kotlin"
+    val greetings = "Hello, $name"
     println(greetings)
 
 //    String literals
     val rawString = """
-        Hello world!,
-        i'am using Kotlin
-        and i'am very excited!!
+        Hello world!
+        i'm using Kotlin
+        and i'am verry excited!
     """.trimIndent()
-
-    val escapedString = "Hello \nkotlin!"
+    val escaptedString = "Hello \nworld!"
     println(rawString)
-    println(escapedString)
+    println(escaptedString)
 
 //    String templating
-    val x = 10
+
+    val x = 5
     val y = 10
-    val result = "sum $x and $y is ${x + y}"
+    val result = "Sum of $x + $y is ${x + y}"
     println(result)
 
 //    String formating
-    val version = 2.0
-    val formatingString = String.format("Hello %s, currently use version %f", name, version)
-    println(formatingString)
+    val version = 2
+    val formatedString = String.format("Hello Kotlin! currently use version %d", version)
+    println(formatedString)
 }
