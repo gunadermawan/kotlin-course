27833f3328ae7a3a50dc943a0850cceee804b8c2
gunadermawan <gunadermawan.official@gmail.com>
2024-07-04 22:46:05 +0700

    course: extensions

--- src/oop/Extensioins.kt
+++ src/oop/Extensioins.kt
@@ -1,2 +1,34 @@
 package oop
 
+fun main() {
+    val str = "Enigma"
+    println(str.lastChar())
+
+    val empty = "null"
+    println(empty.isNullOrEmpty())
+
+    println(divide(10, 3))
+}
+
+// extension
+fun String.lastChar(): Char {
+    return this[this.length - 1]
+}
+
+
+// nullable receiver
+fun String?.isNullOrEmpty(): Boolean {
+    return this == null || this.isEmpty()
+}
+
+// exception handling
+fun divide(a: Int, b: Int): Int {
+    return try {
+        a / b
+    } catch (e: ArithmeticException) {
+        println("error attempted")
+        0
+    } finally {
+        println("division attempted")
+    }
+}
