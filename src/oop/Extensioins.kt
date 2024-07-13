ce6ec6ae46aec20b4ec336197d5624b98d09cf53
gunadermawan <gunadermawan.official@gmail.com>
2024-07-13 11:37:05 +0700

    course: this for object reference and function type

--- src/oop/Extensioins.kt
+++ src/oop/Extensioins.kt
@@ -10,6 +10,23 @@ fun main() {
     println(divide(10, 3))
 }
 
+// this expalin
+
+class Phones(val brand: String, val battery: String) {
+    fun introduce() {
+        println("my brand phone is${this.brand} and battery is ${this.battery}")
+    }
+}
+
+class Cars(brand: String) {
+    private val brand: String
+
+    init {
+        this.brand = brand
+    }
+}
+
+
 // extension
 fun String.lastChar(): Char {
     return this[this.length - 1]
